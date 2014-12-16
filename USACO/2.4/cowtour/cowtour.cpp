efbd4b582c6cb12c9ae4686661f406a4d69281be
jer22 <shijieyywd@gmail.com>
2014-12-16 12:44:27 +0800

    small change

--- USACO/2.4/cowtour/cowtour.cpp
+++ USACO/2.4/cowtour/cowtour.cpp
@@ -48,10 +48,14 @@ int main() {
 		}
 		getchar();
 	}
+
+	
 	for (int k = 1; k <= n; k++)
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++)
 				map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
+
+
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
 			if (map[i][j] != INF)
