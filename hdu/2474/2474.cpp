c116b4941bf561a9be524b988614ad78c719647e
jer22 <shijieyywd@gmail.com>
2015-01-06 19:40:10 +0800

    treedp day3

--- hdu/2474/2474.cpp
+++ hdu/2474/2474.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 int n, m;
-int allo[50005][4];
-int reqe[50005][4];
+int allocation[50005][4];
+int request[50005][4];
 long long pool[4];
 int vis[50005];
 int main() {
@@ -15,27 +15,27 @@ int main() {
 	while (~scanf("%d %d", &n, &m)) {
 		for (int i = 1; i <= m; i++)
 			for (int j = 1; j <= n; j++)
-				scanf("%d", &allo[j][i]);
+				scanf("%d", &allocation[j][i]);
 		for (int i = 1; i <= m; i++)
 			for (int j = 1; j <= n; j++)
-				scanf("%d", &reqe[j][i]);
+				scanf("%d", &request[j][i]);
 		for (int i = 1; i <= m; i++)
 			scanf("%I64d", &pool[i]);
 		memset(vis, 0, sizeof(vis));
 		int cnt = 0;
-		for (int i = 1; i <= n; i++) {
+		while (true) {
 			bool flag = false;
 			for (int j = n; j >= 1; j--) {
 				if (vis[j]) continue;
 				bool can = true;
 				for (int k = 1; k <= m; k++)
-					if (reqe[j][k] > pool[k]) can = false;
+					if (request[j][k] > pool[k]) can = false;
 				if (can) {
 					cnt++;
 					vis[j] = 1;
 					flag = true;
 					for (int k = 1; k <= m; k++)
-						pool[k] += allo[j][k];
+						pool[k] += allocation[j][k];
 				}
 			}
 			if (cnt == n) {
