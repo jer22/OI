83315e2d9e5838a699868cf6f00deb409653d5de
jer22 <shijieyywd@gmail.com>
2015-04-14 11:23:03 +0800

    fuck this splay

--- bzoj/splay.cpp
+++ bzoj/splay.cpp
@@ -1,246 +1,242 @@
-#include <iostream>
+
+
 #include <cstdio>
-#include <algorithm>
 #include <cstring>
+#include <algorithm>
+#include <iostream>
 
-/**
-	伸展树节点
+#define MAXN 1000005
+#define INF 1000000000
 
-	value: 节点保存的值
-	left: 左子树
-	right: 右子树
-*/
-struct SplayNode {
-	int value;
-	SplayNode *left;
-	SplayNode *right;
-};
+using namespace std;
 
-/**
-	右旋
+int n, q;
+int sz, root;
+int arr[MAXN], id[MAXN];
+int v[MAXN];
+int fa[MAXN], siz[MAXN], sum[MAXN], tr[MAXN][2];
+bool rev[MAXN], tag[MAXN];
+int rec[MAXN];
+int lmax[MAXN], rmax[MAXN], mmax[MAXN];
 
-	@param k: 当前顶点
-	@return k1: 新顶点
-*/
-SplayNode * rotateRight(SplayNode *k) {
-	// 将k的左子树作为新的顶点
-	SplayNode *k1 = k -> left;
-	// 将原左子树的右子树移到k的左子树
-	k -> left = k1 -> right;
-	// k作为新树的右子树
-	k1 -> right = k;
-	return k1;
+void pushUp(int rt) {
+    int l = tr[rt][0], r = tr[rt][1];
+    siz[rt] = siz[l] + siz[r] + 1;
+    sum[rt] = sum[l] + sum[r] + v[rt];
+    mmax[rt] = max(mmax[l], mmax[r]);
+    mmax[rt] = max(mmax[rt], lmax[r] + v[rt] + rmax[l]);
+    lmax[rt] = max(lmax[l], sum[l] + v[rt] + lmax[r]);
+    rmax[rt] = max(rmax[r], sum[r] + v[rt] + rmax[l]);
 }
 
-/**
-	左旋
-
-	@param k: 当前顶点
-	@return k1: 新顶点
-*/
-SplayNode * rotateLeft(SplayNode *k) {
-	// 将k的右子树作为新顶点
-	SplayNode *k1 = k -> right;
-	// 将原右子树的左子树作为k的右子树
-	k -> right = k1 -> left;
-	// k作为新树的左子树
-	k1 -> left = k;
-	return k1;
+void pushDown(int rt) {
+    int l = tr[rt][0], r = tr[rt][1];
+    if (tag[rt]) {
+        tag[rt] = rev[rt] = 0;
+        if (l) {
+            tag[l] = 1;
+            v[l] = v[rt];
+            sum[l] = v[rt] * siz[l];
+        }
+        if (r) {
+            tag[r] = 1;
+            v[r] = v[rt];
+            sum[r] = v[rt] * siz[r];
+        }
+        if (v[rt] >= 0) {
+            if (l) lmax[l] = rmax[l] = mmax[l] = sum[l];
+            if (r) lmax[r] = rmax[r] = mmax[r] = sum[r];
+        } else {
+            if (l) lmax[l] = rmax[l] = 0, mmax[l] = v[rt];
+            if (r) lmax[r] = rmax[r] = 0, mmax[r] = v[rt];
+        }
+    }
+    if (rev[rt]) {
+        rev[rt] = 0;
+        rev[l] ^= 1;
+        rev[r] ^= 1;
+        swap(lmax[l], rmax[l]);
+        swap(lmax[r], rmax[r]);
+        swap(tr[l][0], tr[l][1]);
+        swap(tr[r][0], tr[r][1]);
+        pushUp(rt);
+    }
 }
 
-/**
-	伸展操作
-
-	将伸展树T中值与value最近的节点调整至树的根部
-	@param T：伸展树指针
-	@param value：元素的值
-	@return T：新树的根
-*/
-SplayNode * splay(SplayNode *T, int value) {
-	// header为左树与右树的根
-	static SplayNode header;
-	header.left = header.right = NULL;
-	// 左树，右树
-	// 右树的所有值都比T大，左树的所有值都比T小
-	// 左树与右树都是伸展树
-	SplayNode *leftTree, *rightTree;
-	leftTree = rightTree = &header;
-
-	// 直到找到value或树空
-	while (T != NULL && value != T -> value) {
-		// 当value小于T的值，说明value在T的左子树中
-		if (value < T -> value) {
-			if (T -> left == NULL) { break; }
-			// 当value小于T的左子树的值，说明value在T的左子树的左子树中
-			// 即zig-zig
-			if (value < T -> left -> value) {
-				// 右旋
-				T = rotateRight(T);
-			}
-			if (T -> left == NULL) { break; }
-			// 右连接
-			// 即将T与T的右子树连接到rightTree的左子树上
-			// T的左子树作为新的T的根
-			rightTree -> left = T;
-			// rightTree的左子树，即移过来T，将作为新的rightTree
-			// 但是注意此时T的左子树并未删去，
-			// 因为下一次的右连接操作将会连接到移过来T的左子树，
-			// 覆盖掉原来的左子树
-			rightTree = T;
-			T = T -> left;
-		}
-		// 否则说明value在T的右子树中
-		else {
-			if (T -> right == NULL) { break; }
-			// 说明value在T的右子树的右子树中
-			// 即zig-zig
-			if (value > T -> right -> value) {
-				// 左旋
-				T = rotateLeft(T);
-			}
-			if (T -> right == NULL) { break; }
-			// 左连接
-			leftTree -> right = T;
-			leftTree = T;
-			T = T -> right;
-		}
-		/*
-			此时还原树，做法如下：
-			将leftTree作为T的左子树，rightTree作为T的右子树
-			T的原左子树作为leftTree的右子树，原右子树作为rightTree的左子树
-		*/
-		// 将T的左子树连接到leftTree的右子树，同时覆盖掉未删去的右子树
-		leftTree -> right = T -> left;
-		// 将T的右子树连接到rightTree的左子树，同时覆盖掉未删去的左子树
-		rightTree -> left = T -> right;
-		// header.right即为最初的leftTree，即整棵leftTree
-		T -> left = header.right;
-		// header.left即为整棵rightTree
-		T -> right = header.left;
-		return T;
-	}
+void rotate(int x, int &k) {
+    int y = fa[x], z = fa[y];
+    int l, r;
+    if (tr[y][0] == x) l = 0;
+    else l = 1;
+    r = l ^ 1;
+    if (y == k) k = x;
+    else {
+        if (tr[z][0] == y) tr[z][0] = x;
+        else tr[z][1] = x;
+    }
+    fa[x] = z;
+    fa[y] = x;
+    fa[tr[x][r]] = y;
+    tr[y][l] = tr[x][r];
+    tr[x][r] = y;
+    pushUp(y);
+    pushUp(x);
 }
 
-/**
-	插入操作
-
-	将一个值插入树中
-	@param T 树
-	@param value 待插入的值
-	@return 新树的根
-*/
-SplayNode * insert(SplayNode *T, int value) {
-	// 新节点
-	static SplayNode* newNode = NULL;
-
-	if (newNode == NULL) {
-		newNode = new SplayNode;
-	}
-	newNode -> value = value;
-
-	// 当T是空树，就返回newNode
-	if(T == NULL) {
-		newNode -> left = newNode -> right = NULL;
-		T = newNode;
-	}
-	// 否则
-	else {
-		// 当value小于T的值，即应将value插入到T的左子树
-		if (value < T -> value) {
-			// 将T的左子树连接到newNode的左子树
-			newNode -> left = T -> left;
-			// newNode的右子树为T与T的右子树
-			newNode -> right = T;
-			T -> left = NULL;
-			// newNode将作为新的树根
-			T = newNode;
-		}
-		// 当value大于T的值，即应将value插入到T的右子树
-		else if (value > T -> value) {
-			// 将T的右子树连接到newNode的右子树
-			newNode -> right = T -> right;
-			// newNode的左子树为T与T的左子树
-			newNode -> left = T;
-			T -> right = NULL;
-			// newNode将作为新的树根
-			T = newNode;
-		}
-		// 否则说明value以存在于T中
-		else {
-			// 销毁newNode
-			delete newNode;
-		}
-	}
-	newNode = NULL;
-	return T;
+void splay(int x, int &k) {
+    while (x != k) {
+        int y = fa[x], z = fa[y];
+        if (y != k) {
+            if (tr[y][0] == x ^ tr[z][0] == y) rotate(x, k);
+            else rotate(y, k);
+        }
+        rotate(x, k);
+    }
 }
 
-/**
-	删除操作
-
-	将指定节点删除
-	@param T：树
-	@param value：要删除的节点的值
-	@return：新树
-*/
-SplayNode * remove(SplayNode *T, int value) {
-	// 新树
-	SplayNode * newTree;
-	// 如果T非空
-	if (T != NULL) {
-		// 将指定节点调整至树根
-		T = splay(T, value);
-		// 当该节点为要删除的节点
-		// 如果不是，说明要删除的节点不再树中
-		if (value == T -> value) {
-			// 如果T的左子树为空，则新树为T的右子树
-			if (T -> left == NULL) { newTree = T -> right; }
-			// 否则
-			else {
-				// 先将T的左子树作为新树
-				newTree = T -> left;
-				// 此时value比newTree中的所有节点都大，
-				// 所以此次伸展将进行多次左旋，使newTree的右子树为空
-				newTree = splay(newTree, value);
-				// 将T的右子树作为newTree的右子树
-				newTree -> right - T -> right;
-			}
-			// 销毁原来的T
-			delete T;
-			// newTree作为T将被返回
-			T = newTree;
-		}
-	}
-	return T;
+int find(int rt, int rank) {
+    if (tag[rt] || rev[rt]) pushDown(rt);
+    int l = tr[rt][0], r = tr[rt][1];
+    if (siz[l] + 1 == rank) return rt;
+    if (siz[l] + 1 > rank) return find(l, rank);
+    return find(r, rank - siz[l] - 1);
 }
 
-/**
-	删除所有节点
-*/
-void removeAll(SplayNode * T) {
-	if (T != NULL) {
-		removeAll(T -> left);
-		removeAll(T -> right);
-		delete T;
-	}
+void build(int l, int r, int rt) {
+    if (l > r) return;
+    int now = id[l], last = id[rt];
+    if (l == r) {
+        fa[now] = last;
+        siz[now] = 1;
+        sum[now] = v[now] = mmax[now] = arr[l];
+        if (arr[l] >= 0) lmax[now] = rmax[now] = arr[l];
+        else lmax[now] = rmax[now] = 0;
+        if (l < rt) tr[last][0] = now;
+        else tr[last][1] = now;
+        return;
+    }
+    int mid = l + r >> 1;
+    now = id[mid];
+    build(l, mid - 1, mid);
+    build(mid + 1, r, mid);
+    fa[now] = last;
+    v[now] = arr[mid];
+    pushUp(now);
+    if (mid < rt) tr[last][0] = now;
+    else tr[last][1] = now;
 }
 
-void print(SplayNode * T) {
-	if(T == NULL)
-		return;
-	print(T -> left);
-	printf("%d\n", T -> value);
-	print(T -> right);
+int getSum(int k, int len) {
+    int x = find(root, k);
+    int y = find(root, k + len + 1);
+    splay(x, root);
+    splay(y, tr[root][1]);
+    int z = tr[y][0];
+    return sum[z];
 }
 
-SplayNode * tree = NULL;
-int main( void ) {
-	int a;
-	for (int i = 0; i < 10; i++) {
-		scanf("%d", &a);
-		tree = insert(tree, a);
-		print(tree);
-	}
+void cover(int k, int len, int c) {
+    int x = find(root, k);
+    int y = find(root, k + len + 1);
+    splay(x, root);
+    splay(y, tr[root][1]);
+    int z = tr[y][0];
+    v[z] = c;
+    tag[z] = 1;
+    sum[z] = v[z] * siz[z];
+    if (c >= 0) lmax[z] = rmax[z] = mmax[z] = sum[z];
+    else lmax[z] = rmax[z] = 0, mmax[z] = v[z];
+    pushUp(y);
+    pushUp(x);
+}
 
-	return 0;
+void reverse(int k, int len) {
+    int x = find(root, k);
+    int y = find(root, k + len + 1);
+    splay(x, root);
+    splay(y, tr[root][1]);
+    int z = tr[y][0];
+    if (!tag[z]) {
+        rev[z] ^= 1;
+        swap(tr[z][0], tr[z][1]);
+        swap(lmax[z], rmax[z]);
+    }
+    pushUp(y);
+    pushUp(x);
+}
+
+void insert(int k, int len) {
+    for (int i = 1; i <= len; i++) {
+        scanf("%d", &arr[i]);
+        if (rec[0]) id[i] = rec[rec[0]--];
+        else id[i] = ++sz;
+    }
+    build(1, len, 0);
+    int x = find(root, k + 1);
+    int y = find(root, k + 2);
+    int z = id[len + 1 >> 1];
+    splay(x, root);
+    splay(y, tr[root][1]);
+    tr[y][0] = z;
+    fa[z] = y;
+    pushUp(y);
+    pushUp(x);
+}
+
+void reclaim(int rt) {
+    if (!rt) return;
+    int l = tr[rt][0], r = tr[rt][1];
+    reclaim(l);
+    reclaim(r);
+    fa[rt] = tr[rt][0] = tr[rt][1] = 0;
+    tag[rt] = rev[rt] = 0;
+    rec[++rec[0]] = rt;
+}
+
+void del(int k, int len) {
+    int x = find(root, k);
+    int y = find(root, k + len + 1);
+    splay(x, root);
+    splay(y, tr[root][1]);
+    int z = tr[y][0];
+    reclaim(z);
+    tr[y][0] = 0;
+    pushUp(y);  
+    pushUp(x);
+}
+
+int main() {
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &arr[i + 1]);
+    for (int i = 1; i <= n + 2; i++)
+        id[i] = ++sz;
+    arr[1] = arr[n + 2] = -INF;
+    mmax[0] = -INF;
+    build(1, n + 2, 0);
+    root = n + 3 >> 1;
+    char op[10];
+    int x, y, z;
+    for (int i = 0; i < q; i++) {
+        scanf("%s", op);
+        if (op[0] == 'R') {
+            scanf("%d %d", &x, &y);
+            reverse(x, y);
+        } else if (op[0] == 'M') {
+            if (op[2] == 'K') {
+                scanf("%d %d %d", &x, &y, &z);
+                cover(x, y, z);
+            } else printf("%d\n", mmax[root]);
+        } else if (op[0] == 'G') {
+            scanf("%d %d", &x, &y);
+            printf("%d\n", getSum(x, y));
+        } else if (op[0] == 'I') {
+            scanf("%d %d", &x, &y);
+            insert(x, y);
+        } else {
+            scanf("%d %d", &x, &y);
+            del(x, y);
+        }
+    }
+    return 0;
 }
